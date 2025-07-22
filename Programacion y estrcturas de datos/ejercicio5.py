53004a73566dd30e3480e8527d953f5344079fdd
victoor-cyber <borjo6789@gmail.com>
2025-07-22 16:26:56 -0500

    ejercicio5.py

--- Programacion y estrcturas de datos/ejercicio5.py
+++ Programacion y estrcturas de datos/ejercicio5.py
@@ -1,14 +1,19 @@
+numero_escondido = 111
 while True:
-
-    num1= int(input("ESCRIBA UN NUMERO CUALQUIERA PARA DESIFRAR EL NUMERO ESCONDIDO"))
-    if num1 <0:
-        print("solo numeros positivos")
-    else:
-        break
-
-while True:
-  if num1 == 111:
-    print("GENIAALLLLLL ACERTASTE")
-  else:
-     print("no es es l numero, sigue intentando")
-     break
+    try:
+        num1= int(input("Ingrese un numero para decifrar el numero escondido:"))
+        if num1 < 40:
+            print("Estas muy lejos del numero escondido")
+        elif num1 < 80:
+            print("Estas casi cerca")
+        elif num1 < 100:
+            print("Estas cerca")
+        elif num1 < 111:
+            print("Estas muy muy cerca")
+        elif num1 > 111:
+            print("Te pasastes del numero escondido")
+        else:
+            print("Felicidades, acertaste el numero econdido era el 111")
+            break
+    except:
+        print("Solo son letras, vuelva y digite:")
