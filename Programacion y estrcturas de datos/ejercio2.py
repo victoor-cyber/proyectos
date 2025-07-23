d0ea99d3cddcb8c101267ef8d93fe26c3de87792
victoor-cyber <borjo6789@gmail.com>
2025-07-23 06:46:48 -0500

    Update ejercio2.py

--- Programacion y estrcturas de datos/ejercio2.py
+++ Programacion y estrcturas de datos/ejercio2.py
@@ -1,10 +1,11 @@
+#Hacer un programa que muestre todos los numeros entre dos numeros que digite el usuario
 while True:
     num1=int(input ("Ingrese un numero cualquiera que no sea negativo"))
     if num1<0:
         print("solo son nuemeros, ni puntos ni letras")
     else:
         break
-
+        
 while True:
     num2=int(input ("Ingrese un numero cualquiera que no sea negativo"))
     if num2 <0:
